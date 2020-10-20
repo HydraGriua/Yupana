c1ab41f0f0b36e5172ab51050add9821d0ab4009
Flavio Saavedra <f.saavedra1810@gmail.com>
2020-10-19 23:00:50 -0500

    First Packages

---
+++ src/main/java/com/acme/yupanaapi/YupanaApiApplication.java
@@ -0,0 +1,13 @@
+package com.acme.yupanaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YupanaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YupanaApiApplication.class, args);
+    }
+
+}
