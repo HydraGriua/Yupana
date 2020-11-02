d16947c2442003d4df7317300d0ca17142baa995
Flavio Saavedra <f.saavedra1810@gmail.com>
2020-11-02 10:59:31 -0500

    Progress

--- src/main/java/com/acme/yupanaapi/resource/SaveUserResource.java
+++ src/main/java/com/acme/yupanaapi/resource/SaveUserResource.java
@@ -1,4 +1,29 @@
 package com.acme.yupanaapi.resource;
 
+import javax.persistence.Lob;
+import javax.validation.constraints.NotNull;
+
 public class SaveUserResource {
+    //TODO: agregar el max length
+    @NotNull
+    private String documentType;
+
+    @NotNull
+    private String documentNumber;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String firstLastname;
+
+    @NotNull
+    private String secondLastname;
+
+    @NotNull
+    private String cellphone;
+
+    @Lob
+    @NotNull
+    private String description;
 }
--- src/main/java/com/acme/yupanaapi/resource/UserResource.java
+++ src/main/java/com/acme/yupanaapi/resource/UserResource.java
@@ -1,4 +1,20 @@
 package com.acme.yupanaapi.resource;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Lob;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
 public class UserResource {
+    private Long id;
+    private String documentType;
+    private String documentNumber;
+    private String name;
+    private String firstLastname;
+    private String secondLastname;
+    private String cellphone;
+    private String description;
 }
