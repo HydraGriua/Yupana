7841172b66154f862fede3fb2f711275dd207bef
Flavio Saavedra <f.saavedra1810@gmail.com>
2020-11-08 19:17:07 -0500

    WalletsController Complete

--- src/main/java/com/acme/yupanaapi/domain/service/WalletService.java
+++ src/main/java/com/acme/yupanaapi/domain/service/WalletService.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public interface WalletService {
 	Wallet createWallet(Wallet entity, Long sellerId, Long userId);
-	Wallet updateWallet(Wallet entity, Long walletId, Long sellerId, Long userId);
+	Wallet updateWallet(Wallet entity, Long walletId, Long sellerId);
 	ResponseEntity<?> deleteWallet(Long walletId);
 	Wallet getWalletById(Long walletId);
 	Wallet getWalletByIdAndUserId(Long walletId, Long userId);
