dd3f253f80a564800965093a8db5790e502bbc79
Flavio Saavedra <f.saavedra1810@gmail.com>
2020-11-16 20:40:41 -0500

    fix

--- src/main/java/com/acme/yupanaapi/controller/UsersController.java
+++ src/main/java/com/acme/yupanaapi/controller/UsersController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "https://yupana.herokuapp.com/")
 @RequestMapping("/api")
 public class UsersController {
 
