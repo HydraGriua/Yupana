d39b45bb9c632d6d7dffde51a67e10ad174dce96
Nicole Silva <nicole_vasquez_silva@hotmail.com>
2020-11-22 22:43:24 -0500

    integration part1

--- src/main/java/com/acme/yupanaapi/controller/CustomersController.java
+++ src/main/java/com/acme/yupanaapi/controller/CustomersController.java
@@ -7,18 +7,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.acme.yupanaapi.domain.model.Flow;
 import com.acme.yupanaapi.domain.model.Subscription;
+import com.acme.yupanaapi.domain.model.Transaction;
+import com.acme.yupanaapi.domain.model.User;
 import com.acme.yupanaapi.domain.model.Wallet;
 import com.acme.yupanaapi.domain.service.FlowService;
 import com.acme.yupanaapi.domain.service.SellerService;
 import com.acme.yupanaapi.domain.service.SubscriptionService;
+import com.acme.yupanaapi.domain.service.TransactionService;
+import com.acme.yupanaapi.domain.service.UserService;
 import com.acme.yupanaapi.domain.service.WalletService;
 import com.acme.yupanaapi.resource.UserWalletResource;
 
 @Controller
-@RequestMapping("mystore/clients")
+@RequestMapping("mystore")
 public class CustomersController {
 	@Autowired
 	SellerService sellerService;
@@ -32,7 +39,13 @@ public class CustomersController {
 	@Autowired
 	FlowService flowService;
 	
-	@GetMapping
+	@Autowired
+	TransactionService transactionService;
+	
+	@Autowired
+	UserService userService; 
+	
+	@GetMapping("/clients")
 	public String viewCustomers(Model model) {
 		try {
 			System.err.println("hola como estas");
@@ -41,7 +54,7 @@ public class CustomersController {
             List<UserWalletResource>  walletsT = new ArrayList<>();
           
             for (Wallet x : wallets) {
-               System.err.println(x.getId());
+               System.out.println(x.getId());
                UserWalletResource resource = flowService.getData(x.getId());
                List<Subscription> lista = subscriptionService.getAllByWalletId(x.getId());
                if(lista.size()>=1) {
@@ -53,11 +66,7 @@ public class CustomersController {
                    resource.setSType(subscription.getSubscriptionType());
                    walletsT.add(resource);
                }
-                       	
-            	System.err.println(walletsT.get(0).getBalance() + " "+ 
-            	walletsT.get(0).getSExpirationDate() + " "  + 
-            	walletsT.get(0).getCreditLine()+" " + walletsT.get(0).getTotalDebt() + " " +  walletsT.get(0).getSAmount() 
-            	+ " " + walletsT.get(0).getType() + " " + walletsT.get(0).getName());
+                       	System.out.println("Id" + walletsT.get(0).getFlowId());
                
             }
 	
@@ -69,6 +78,27 @@ public class CustomersController {
 		}
 		
 		return "/mystore/clients";
+	}
+	
+	@GetMapping("/client-details")
+	public String viewLearnMore(@RequestParam(name = "id", required = false) int id, Model model) {
+		try {
+			System.out.println("Id del cliente" + " " + id);
+			Wallet walletT = walletService.getWalletById(id);
+			Flow flowT = flowService.getLastFlow(walletT.getId());
+			List<Transaction> transacionesT = transactionService.getAllByFlowId(flowT.getId());
+			if(transacionesT != null) {
+				model.addAttribute("wallets", walletT);
+				model.addAttribute("flows", flowT);
+				model.addAttribute("transacionesT", transacionesT);	
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+		
+		return "/mystore/client-details";
 	}
 	
 }
